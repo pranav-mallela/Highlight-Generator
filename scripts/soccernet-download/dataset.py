e17d31179830dd4948a2912bc506a7d5e7f06209
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2025-03-29 11:25:46 -0400

    Fix script and update requirements

--- scripts/soccernet-download/dataset.py
+++ scripts/soccernet-download/dataset.py
@@ -1,6 +1,11 @@
+import os
+
+from dotenv import load_dotenv
 from SoccerNet.Downloader import SoccerNetDownloader
+
+load_dotenv()
 
 local_dir = "/scratch/eecs545w25_class_root/eecs545w25_class/highlights/SoccerNet"
 mySoccerNetDownloader = SoccerNetDownloader(LocalDirectory=local_dir)
-mySoccerNetDownloader.password = "s0cc3rn3t"
+mySoccerNetDownloader.password = os.getenv('SOCCERNET_PASS')
 mySoccerNetDownloader.downloadGames(files=["1_720p.mkv", "2_720p.mkv", "video.ini", "Labels-v2.json"], split=["train","valid","test"])
