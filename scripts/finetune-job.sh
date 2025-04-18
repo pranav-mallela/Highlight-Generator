bab18388051cd73d7c24e808be1130a718be7122
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2025-04-17 21:01:49 -0400

    Enable gpu slurm partition

--- scripts/finetune-job.sh
+++ scripts/finetune-job.sh
@@ -10,7 +10,7 @@
 #SBATCH --mem-per-cpu=32GB
 #SBATCH --time=8:00:00
 #SBATCH --account=eecs545w25_class
-#SBATCH --partition=spgpu
+#SBATCH --partition=spgpu,gpu
 #SBATCH --output=/home/%u/logs/%x-%j.log
 # COMMENT:The application(s) to execute along with its input arguments and options:
 
